79f6a38407183b99378c1a5ac54e4e1ba57e1542
Ren <laurence.apuya.p@gmail.com>
2022-04-13 21:04:07 +0200

    added factory init

--- src/main/java/es/ite/felizmente/FelizMenteApplication.java
+++ src/main/java/es/ite/felizmente/FelizMenteApplication.java
@@ -1,14 +1,27 @@
 package es.ite.felizmente;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class FelizMenteApplication {
+public class FelizMenteApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(FelizMenteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("FelizMente");
+		EntityManager em = factory.createEntityManager();
+		
 	}
 	
 }
